f6ae709694dce8739f94f8ad8af5f48a508987b5
Markus Paasovaara <markus.paasovaara@futurice.com>
2017-09-30 10:01:16 +0300

    Arduino code for proto1

---
+++ Arduino/ControlMosfet/ControlMosfet.ino
@@ -0,0 +1,21 @@
+/**
+ * Simplest code ever, read if button pressed -> set pin state to outputpin (which controls the mosfet)
+ */
+
+const int switchPin = 2;
+const int outputPin = 9;
+int switchState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(outputPin, OUTPUT);
+  pinMode(switchPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchState = digitalRead(switchPin);
+  int outputState = (switchState == HIGH) ? HIGH : LOW;
+  digitalWrite(outputPin, outputState);
+  
+}
